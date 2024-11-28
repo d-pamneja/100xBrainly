52a60840046dbabf64a38b4e24be146cbba086bf
d-pamneja <dpamneja@gmail.com>
2024-11-28 14:09:50 +0530

    Deployment

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import app from "./app";
 // Connect to MongoDB first
 connect(process.env.MONGODB_URI!)
   .then(() => {
-    console.log("Server opened and connected to MongoDB.");
+    console.log("Server set and connected to MongoDB.");
   })
   .catch((err) => console.log("MongoDB connection error: ", err));
 
